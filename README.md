6a3d6499054ce5ed2d8cede8aaf7bf611bca14ea
weisinator <weisinator@gmail.com>
2019-06-05 20:25:30 -0500

    v1.0.1: rounded some output

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # subwooferjs
 The goal of this project is an open source javascript subwoofer library to be included in other open source projects.
+# v1.0.1 updates
+Rounded numbers off to keep things simple.
--- src/subwoofer.js
+++ src/subwoofer.js
@@ -1,5 +1,6 @@
 const circle = require('./circle');
 const convert = require('convert-units');
+const round = require('round');
 
 function subwoofer(d, e, dc = 1) {
     this.driver = d;
@@ -7,26 +8,26 @@ function subwoofer(d, e, dc = 1) {
     this.portArea = -1;
     this.driverCount = dc;
     this.minimumPortArea = function(){
-        return circle.diameterToArea(
+        return round(circle.diameterToArea(
             convert(
                 Math.sqrt(
                     this.enclosure.fb * this.driver.peakDisplacementVolume()*this.driverCount)
-                    ).from('m').to('cm'));
+                    ).from('m').to('cm')),0.1);
     }
     this.portLength = function(){
         if(this.portArea == -1){
             this.portArea = this.minimumPortArea();
         }
-        return 23562.5*Math.pow(circle.areaToDiameter(this.portArea),2)/(this.enclosure.vb*Math.pow(this.enclosure.fb,2))
+        return round(23562.5*Math.pow(circle.areaToDiameter(this.portArea),2)/(this.enclosure.vb*Math.pow(this.enclosure.fb,2)),0.1);
     }
     this.frequencyResponse = function(f){
         if(this.enclosure.fb == -1){
-            return this.sealedFrequencyResponse(f);
+            return round(this.sealedFrequencyResponse(f),0.01);
         }
-        return this.portedFrequencyResponse(f);
+        return round(this.portedFrequencyResponse(f),0.01);
     }
     this.setPortArea = function(pa){
-        this.portArea = pa;
+        this.portArea = round(pa,0.01);
     }
     
     this.portedFrequencyResponse = function(f){
